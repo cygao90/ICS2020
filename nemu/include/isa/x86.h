ead1cd182b9c66429e2bd19557bd357e373ef92d
WCTPD <1426469360@qq.com>
2020-10-09 08:31:28 -0700

    finish reg_test and remove "-Werror" from Makefile

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -18,18 +18,31 @@
  */
 
 typedef struct {
+  /*
   struct {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
   } gpr[8];
+  */
+  struct {
+    union {
+      union {
+        uint32_t _32;
+        uint16_t _16;
+        uint8_t _8[2];
+      } gpr[8];
+      struct {
+        /* Do NOT change the order of the GPRs' definitions. */
 
-  /* Do NOT change the order of the GPRs' definitions. */
-
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+         * in PA2 able to directly access these registers.
+         */
+        rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+      };
+    };
+  }; 
+  
 
   vaddr_t pc;
 } x86_CPU_state;
